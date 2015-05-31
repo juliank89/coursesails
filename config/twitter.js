569ffc117f43c99743ea90c03c91e364a7b3b259
julian toledo <juliank89@gmail.com>
2015-05-31 01:34:49 -0500

    ok twitter

---
+++ config/twitter.js
@@ -0,0 +1,5 @@
+module.exports.twitter = {
+	consumerKey:'LbANUz5vLF84lMvwPWyfgvO9p',
+	consumerSecret:'AWjC74dczxhZjH4hFMXbmPadVe4ptNrOcl1OgI3sq8ZrQNRwwF',
+	callbackUrl:'https://coursesailsjt.herokuapp.com/'
+};
